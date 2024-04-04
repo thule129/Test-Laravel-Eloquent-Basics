a768448ae74e96ce4eaf9c9e90b2a848e04fb542
thule129 <baekhyunee129@gmail.com>
2024-04-05 00:59:12 +0700

    first attempt

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Stat;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    protected static function booted() {
+        static::created(function (Project $project) {
+            $stat = Stat::first();
+            $stat->projects_count += 1;
+            $stat->save();
+        });
+    }
 }
